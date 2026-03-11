f552ac556246667573037eb6f7e8a9baeeed9f07
danishgeminiyt-create <danishgeminiyt@gmail.com>
2026-03-11 12:32:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -702,3 +702,13 @@ bot.on('polling_error', (err) => {
 });
 
 console.log('FaceSwap bot is running...');
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running");
+}).listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
